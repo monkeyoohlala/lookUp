3cb9cacb643dbacdd64566184596c162ef98e391
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-10-06 22:20:07 -0700

    Add module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.monkeyoohlala.lookup {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires org.seleniumhq.selenium.api;
+    requires org.seleniumhq.selenium.chrome_driver;
 
 
     opens com.monkeyoohlala.lookup to javafx.fxml;
